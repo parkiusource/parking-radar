e8eb4d8cd0e5048ed1682e7b2b0247f3fe75e3c9
CamiloLeonP <leon971010@gmail.com>
2024-05-14 23:43:07 -0500

    Configure PORT environment variable

--- cmd/main.go
+++ cmd/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"os"
+
 	"github.com/CamiloLeonP/parking-radar/internal/app/handler"
 	"github.com/gin-gonic/gin"
 )
@@ -9,8 +11,15 @@ func main() {
 
 	router := gin.Default()
 
+	gin.SetMode(gin.ReleaseMode)
+
 	router.POST("/ping", handler.PinHandler)
 
-	router.Run(":8080")
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080" // Default port if not specified
+	}
+
+	router.Run(":" + port)
 
 }
