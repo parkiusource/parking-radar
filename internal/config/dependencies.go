1f6147f4a7554e822b20c0b1eb47cddbd605a3fc
CamiloLeonP <leon971010@gmail.com>
2024-08-28 22:10:05 -0500

    precommit file modified

--- internal/config/dependencies.go
+++ internal/config/dependencies.go
@@ -4,6 +4,7 @@ import (
 	"github.com/CamiloLeonP/parking-radar/internal/app/adapter/input/handler"
 	"github.com/CamiloLeonP/parking-radar/internal/app/adapter/output/db"
 	"github.com/CamiloLeonP/parking-radar/internal/app/usecase"
+	db2 "github.com/CamiloLeonP/parking-radar/internal/db"
 )
 
 type Handlers struct {
@@ -18,7 +19,7 @@ func SetupDependencies() *Handlers {
 	userHandler := handler.NewUserHandler(userUseCase)
 
 	// Configuración para ParkingLot
-	parkingLotRepository := &db.ParkingLotRepositoryImpl{}
+	parkingLotRepository := &db.ParkingLotRepositoryImpl{DB: db2.DB}
 	parkingLotUseCase := usecase.NewParkingLotUseCase(parkingLotRepository)
 	parkingLotHandler := handler.NewParkingLotHandler(parkingLotUseCase)
 
