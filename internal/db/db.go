3597009b7bc144f533c701d703c8e511b87c4ee2
CamiloLeonP <leon971010@gmail.com>
2024-09-21 21:21:18 -0500

    refactor database connection v3

--- internal/db/db.go
+++ internal/db/db.go
@@ -2,6 +2,7 @@ package db
 
 import (
 	"log"
+	"os"
 	"time"
 
 	"gorm.io/driver/postgres"
@@ -11,17 +12,19 @@ import (
 var DB *gorm.DB
 
 func ConnectDatabase() {
-	dsn := "host=localhost user=myuser password=mypassword dbname=parking_system_db port=5432 sslmode=disable TimeZone=Asia/Shanghai"
-	database, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	dsn := os.Getenv("DATABASE_URL")
+	if dsn == "" {
+		log.Fatal("DATABASE_URL environment variable is not set")
+	}
 
+	database, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
 		log.Fatal("Failed to connect to database:", err)
 	}
 
 	sqlDB, err := database.DB()
-
 	if err != nil {
-		log.Fatal("Falied to get DB instance from GORM:", err)
+		log.Fatal("Failed to get DB instance from GORM:", err)
 	}
 
 	sqlDB.SetMaxIdleConns(10)
